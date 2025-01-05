a531aa04b23ef2a9c310121f4ef2776e1333f8d6
pnttrrv <ndhkptr@gmail.com>
2025-01-06 00:54:13 +0700

    Fixing navbar and hover on mobile

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const home = document.querySelector('.home');
 const prods = document.querySelector('.prods');
 const testi = document.querySelector('.testi');
 const contact = document.querySelector('.kontak');
+const cookies = document.querySelector('.coo');
+const fudgy = document.querySelector('.fud');
+const tart = document.querySelector('.tar');
+const pchoc = document.querySelector('.pc');
+const bun = document.querySelector('.pk');
+const infocoo = document.querySelector('.product .info.coo');
+const infofud = document.querySelector('.product .info.fud');
+const infotar = document.querySelector('.product .info.tar');
+const infopc = document.querySelector('.product .info.pc');
+const infobun = document.querySelector('.product .info.pk');
 
 toggle.addEventListener('click', function () {
   list.classList.toggle('slide-down');
@@ -27,4 +37,50 @@ testi.addEventListener('click', function () {
 contact.addEventListener('click', function () {
   list.classList.toggle('slide-down');
   toggle.checked = false;
+});
+
+// child1.forEach((inpo) => {
+//   inpo.addEventListener('click', function () {
+//     info.classList.toggle('show-details');
+//   });
+// });
+
+cookies.addEventListener('click', function () {
+  infocoo.classList.add('show-details');
+});
+
+infocoo.addEventListener('click', function () {
+  infocoo.classList.remove('show-details');
+});
+
+fudgy.addEventListener('click', function () {
+  infofud.classList.add('show-details');
+});
+
+infofud.addEventListener('click', function () {
+  infofud.classList.remove('show-details');
+});
+
+tart.addEventListener('click', function () {
+  infotar.classList.add('show-details');
+});
+
+infotar.addEventListener('click', function () {
+  infotar.classList.remove('show-details');
+});
+
+pchoc.addEventListener('click', function () {
+  infopc.classList.add('show-details');
+});
+
+infopc.addEventListener('click', function () {
+  infopc.classList.remove('show-details');
+});
+
+bun.addEventListener('click', function () {
+  infobun.classList.add('show-details');
+});
+
+infobun.addEventListener('click', function () {
+  infobun.classList.remove('show-details');
 });
